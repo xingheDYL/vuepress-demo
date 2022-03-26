5ddccf3ed808c1fe709232b252b7858f75d85a1e
xinghe <1329749225@qq.com>
2022-03-26 10:49:48 +0800

    feat: pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,22 @@ const moment = require('moment');
 moment.locale("zh-cn") //显示中国的时间格式
 
 module.exports = {
-    base: "/vuepress/",
+    // base: "/vuepress/",
     title: "星河",
     description: "星河2",
     head: [
+        ["link", { rel: "icon", href: "/assets/images/favicon.ico" }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+
         // ['meta', { name: "baidu_union_verify", content: "a0036ccf0b657813fb236f80f376c429" }],
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ["link", { rel: "icon", href: "/assets/images/favicon.ico" }],
         ['meta', { name: 'Keywords', content: '段先生,段先生的主页,vuepress,VuePress,vuepress.dyl.fit,VuePress.dyl.fit' }],
         // 作者
         ['meta', { name: 'author', content: '星河' }],
@@ -55,10 +64,20 @@ module.exports = {
     },
     plugins: [
         [
-            '@vuepress/last-updated',
-            {
+            '@vuepress/last-updated', {
                 transformer: (timestamp) => {
                     return moment(timestamp).format("LLLL")
+                }
+            }
+        ],
+        [
+            '@vuepress/pwa', {
+                serviceWorker: true,
+                updatePopup: {
+                    '/zh/': {
+                        message: "发现新内容可用",
+                        buttonText: "刷新"
+                    }
                 }
             }
         ]
