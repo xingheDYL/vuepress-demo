babc19b7e197bcfd236d9e85b138e3fbc4aa17be
xinghe <1329749225@qq.com>
2022-03-26 12:12:05 +0800

    feat: vssue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,22 +62,27 @@ module.exports = {
             ]
         }
     },
-    plugins: [
-        [
-            '@vuepress/last-updated', {
-                transformer: (timestamp) => {
-                    return moment(timestamp).format("LLLL")
-                }
+    plugins: {
+        '@vuepress/last-updated': {
+            transformer: (timestamp) => moment(timestamp).format("LLLL")
+        },
+        '@vuepress/pwa': {
+            serviceWorker: true,
+            updatePopup: {
+                message: "发现新内容可用",
+                buttonText: "刷新"
             }
-        ],
-        [
-            '@vuepress/pwa', {
-                serviceWorker: true,
-                updatePopup: {
-                    message: "发现新内容可用",
-                    buttonText: "刷新"
-                }
-            }
-        ]
-    ]
+        },
+        '@vssue/vuepress-plugin-vssue': {
+            // 设置 `platform` 而不是 `api`
+            platform: 'github-v4',
+
+            // 其他的 Vssue 配置
+            owner: 'xingheDYL',
+            repo: 'vuepress',
+            clientId: '8f9485afcb3e4e619d6a',
+            clientSecret: '7246f5a1ae9eddd91cdf535001178d8f7820482f',
+            autoCreateIssue: true
+        }
+    }
 }
